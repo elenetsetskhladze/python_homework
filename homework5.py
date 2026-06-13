5b813a444b7ed7f738f23fcefbe872935d471b64
elenetsetskhladze <elene.919191@gmail.com>
2026-06-13 15:10:15 +0400

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,15 @@
+try:
+    number1 = float(input("Enter the first number: "))
+    number2 = float(input("Enter the second number: "))
+    
+    print(number1 / number2)
+
+except ValueError:
+    print("Please enter valid numbers!")
+except ZeroDivisionError:
+    print("Cannot divide by zero!")
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
+else:
+    print('success')
+

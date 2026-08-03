f25d4c4bb4cdd05ec308641af030a4e6e78f9246
elenetsetskhladze <elene.919191@gmail.com>
2026-08-03 21:54:53 +0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,168 +1,308 @@
 import json
+from datetime import date
+
+CURRENT_YEAR = date.today().year
 
 
-#Logging in
-def load_users():
-    try:
-        with open("user.json", "r") as file:
-            return json.load(file)
-    except FileNotFoundError:
-        print("user.json not found.")
-        return []
+class Book:
+    def __init__(self, title, author, year, quantity):
+        self.title = title
+        self.author = author
+        self.year = year
+        self.quantity = quantity
+
+    @property
+    def title(self):
+        return self.__title
+    @title.setter
+    def title(self, value):
+        if not isinstance(value, str):
+            raise ValueError("Title must be string")
+
+        if not value.strip():
+            raise ValueError("Title cannot be empty.")
+
+        self.__title = value.strip()
+
+        
+
+    @property
+    def author(self):
+        return self.__author
+
+    
+    @author.setter
+    def author(self, value):
+        if not isinstance(value, str):
+            raise ValueError("Author must be a string.")
+
+        if not value.strip():
+            raise ValueError("Author cannot be empty.")
+
+        self.__author = value.strip()
+
+    @property
+    def year(self):
+        return self.__year
 
 
-def save_users(users):
-    with open("user.json", "w") as file:
-        json.dump(users, file, indent=4)
+    @year.setter
+    def year(self, new_year):
+        if isinstance(new_year, int) and 1000 <= new_year <= CURRENT_YEAR:
+            self.__year = new_year
+        else:
+            raise ValueError("Invalid year.")
 
-def logging_in(users):
+    @property
+    def quantity(self):
+        return self.__quantity
+
+    @quantity.setter
+    def quantity(self, value):
+        if isinstance(value, int) and value >= 0:
+            self.__quantity = value
+        else:
+            raise ValueError("Quantity must be positive number")
 
 
-    print("=" * 40)
-    name = input("name: ")
-    surname = input("surname: ")
-    password = input("password: ")
-    print("=" * 40)
+    def to_dict(self):
+        return {"title": self.title, "author": self.author,"year": self.year, "quantity": self.quantity}
 
 
+    def show(self):
+        print("-" * 40)
+        print(f"Title : {self.title}")
+        print(f"Author: {self.author}")
+        print(f"Year  : {self.year}")
+        print(f"quantity : {self.quantity}")
+        print("-" * 40)
 
-    for user in users:
-        if user["name"] == name and user["surname"] == surname and user["password"] == password:
-            return user
-    return None
 
- #Choose the action
-def atm_menu_bar(user, all_users):
-    while True:
+class BookManager:
+    def __init__(self):
+        self.books = []
+        self.file_name = "books.json"
+        self.import_books()
 
-        print("=" * 40)
-        print("1. Check balance: ")
-        print("2. Deposit the money:")
-        print("3. withdraw the money:")
-        print("4. Exchange currency:")
-        print("5. Exit")
-        print("=" * 40)
+    def import_books(self):
+        try:
+            with open(self.file_name, "r") as file:
+                data = json.load(file)
 
-        choice = input("choose the action: ")
-#cheching balance
+                for book in data:
+                    
+                    new_book = Book(
+                        book["title"],
+                        book["author"],
+                        book["year"],
+                        book["quantity"]
+                    )
 
-        if choice == "1":
-            print("GEL:", user["balance"]["GEL"])
-            print("USD:", user["balance"]["USD"])
+                    self.books.append(new_book)
+        except FileNotFoundError:
+            self.books = []
+        except (json.JSONDecodeError, KeyError):
+            print("Invalid data inside books.json")
+            self.books = []
 
-#deposit money
-        elif choice == "2":
-            currency = input("Currency (GEL/USD): ").upper()
+    def store_books(self):
+
+        data = []
+
+        for book in self.books:
+            data.append(book.to_dict())
+
+
+        with open(self.file_name, "w") as file:
+            json.dump(data, file, indent=4)
+
+
+    def find_book(self, title):
+        for book in self.books:
+            if book.title.lower() == title.lower():
+                return book
+        return None
+
+    def register_book(self):
+        print("----- Add book -----")
+
+        while True:
+            title = input("Enter title: ").strip()
+
+            if title:
+                break
+
+            print("Title cannot be empty")
+
+        while True:
+            author = input("Enter author: ").strip()
+
+            if author:
+                break
+
+            print("author cannot be empty")
+
+        while True:
+            try:
+                year = int(input("Enter publication year: "))
+
+                if 1000 <= year <= CURRENT_YEAR:
+                    break
+
+                print("Invalid year.")
+
+            except ValueError:
+                print("Enter a valid number.")
+
+        while True:
+            try:
+                quantity = int(input("Enter quantity: "))
+
+                if quantity >= 0:
+                    break
+                print("Quantity cannot be negative.")
+            except ValueError:
+                print("Enter a valid number.")
+
+
+        for book in self.books:
+            if (
+                book.title.lower() == title.lower()
+                and book.author.lower() == author.lower()
+            ):
+                print("Book already exist.")
+                return
+
+        
+
+        self.books.append(Book(title, author, year, quantity))
+        self.store_books()
+
+        print("Book added successfully")
+
+    def show_books(self):
+        print("---- Book list ----")
+
+        if not self.books:
+            print("No books found")
+            return
+
+        for book in self.books:
+            book.show()
+
+    def lookup_books(self):
+        print("---- Search book ----")
+
+        title = input("Enter book title: ").strip()
+
+        book = self.find_book(title)
+
+        if book:
+            book.show()
+        else:
+            print("book not found.")
+
+    def delete_books(self):
+        print("------ delete book ------")
+
+        title = input("Enter title: ").strip()
+        book = self.find_book(title)
+
+        if book:
+            self.books.remove(book)
+            self.store_books()
+            print("book removed")
+        else:
+            print("book not found.")
+
+    def sort_books(self):
+        self.books.sort(key=lambda book: book.title.lower())
+        self.store_books()
+        print("Books sorted alphabetically.")
+
+    def borrow_book(self):
+        title = input("Enter title: ").strip()
+
+        book = self.find_book(title)
+
+        if not book:
+            print("We dont have this book")
+            return
+        
+        if book.quantity == 0:
+            print("This book is currently unavailable")
+            return
+
+        book.quantity -= 1
+        self.store_books()
+        print("Exported successfully")
+
+
+    def return_book(self):
+        title = input("Enter title: ").strip()
+
+        book = self.find_book(title)
+
+        if not book:
+            print("We don't have this book")
+            return
+
+        book.quantity += 1
+        self.store_books()
+        print("Book returned successfully.")
+
+    def menu(self):
+        while True:
+            print("======== BOOK MANAGEMENT =======")
+            print("1. register Book")
+            print("2. Show All Books")
+            print("3. Look up Book")
+            print("4. Delete Book")
+            print("5. Sort Books")
+            print("6. Borrow Book")
+            print("7. Return book")
+            print("8. Exit")
 
             try:
-                amount = float(input("amount of the money to deposit: "))
-            except ValueError:
-                print("Enter valid number")
-                continue
-
-                
-            if currency in user["balance"]:
-                user["balance"][currency] += amount
-                save_users(all_users)
-                print("New balance:", user["balance"])
-            else:
-                print("invalid currency")
-
-#withdraw money
-
-        elif choice == "3":
-            currency = input("Currency (GEL/USD): ").upper()
-
-            try:
-                amount = float(input("amount of the money to withdraw: "))
-            except ValueError:
-                print("Please enter a valid number.")
-                continue
-
-            if currency not in user["balance"]:
-                print("invalid currency")
-            elif amount <= user["balance"][currency]:
-                user["balance"][currency] -= amount
-                save_users(all_users)
-                print("new balance:", user["balance"])
-            else:
-                print("you don't have enough money")
-
-#exchange money
-
-        elif choice == "4":
-            USD_TO_GEL = 2.7
-
-            print("1. USD -> GEL")
-            print("2. GEL -> USD")
-
-            convert_choice = input("choose: ")
-#from USD to GEL
-
-            if convert_choice == "1":
-                try:
-                    usd_amount = float(input("How many USD do you want to convert? "))
-                except ValueError:
-                    print("Enter valid number")
-                    continue
-
-                if usd_amount <= user["balance"]["USD"]:
-                    gel_amount = usd_amount * USD_TO_GEL
-
-                    user["balance"]["USD"] -= usd_amount
-                    user["balance"]["GEL"] += gel_amount
-
-                    save_users(all_users)
-
-                    print(f"Balance: {gel_amount} GEL")
-                else:
-                    print("Not enough USD")
-
-#from GEL to USD
-
-            elif convert_choice == "2":
-                try:
-                    gel_amount = float(input("How many GEL do you want to convert? "))
-                except ValueError:
-                    print("Enter valid number")
-                    continue
-                
-                if gel_amount <= user["balance"]["GEL"]:
-                    usd_amount = gel_amount / USD_TO_GEL
+                choice = input("Choose: ")
+            except EOFError:
+                print("No input available. Exiting.")
+                break
 
 
-                    user["balance"]["GEL"] -= gel_amount
-                    user["balance"]["USD"] += usd_amount
-
-                    save_users(all_users)
-
-                    print(f"Balance: {usd_amount:.2f} USD")
-                else:
-                   print("Not enough GEL.")
-
+            if choice == "1":
+                self.register_book()
+            elif choice == "2":
+                self.show_books()
+            elif choice == "3":
+                self.lookup_books()
+            elif choice == "4":
+                self.delete_books()
+            elif choice == "5":
+                self.sort_books()
+            elif choice == "6":
+                self.borrow_book()
+            elif choice == "7":
+                self.return_book()
+            elif choice == "8":
+                print("Program completed.")
+                break
             else:
                 print("Invalid choice.")
 
-#exit                 
-        elif choice == "5":
-                    print("thank you, have a nice day")
-                    break
-        print("=" * 40)
-
-all_users = load_users()       
-user = logging_in(all_users)
 
 
-if user:
-    print("You Logged in successfully")
-    print("Balance:", user["balance"])
-    atm_menu_bar(user, all_users)
-
-else:
-    print("Wrong credentials")
-
-
+if __name__ == "__main__":
+    manager = BookManager()
+    manager.menu() 
 
 
         
+
+
+
+
+
+
+
         
